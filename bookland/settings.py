44192e23a1000727c5e08a52a31945e02749d828
kevin <kevinbar98@gmail.com>
2023-02-19 01:45:53 +0300

    + postgresql connection

--- bookland/settings.py
+++ bookland/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bookland.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookland',
+        'USER': 'bookland',
+        'PASSWORD': 'bookland',
+        'HOST': 'localhost',
+        'POSRT': 5432
     }
 }
 
